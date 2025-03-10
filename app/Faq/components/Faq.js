b5a5ba685a5a5515d0888ff9e336e4be1f6bffc0
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-03-10 22:59:56 +0700

    perbaiki spasi pada kalimat dan perbaiki email bmkg

--- app/Faq/components/Faq.js
+++ app/Faq/components/Faq.js
@@ -1,24 +1,16 @@
 "use client";
 import { useState } from "react";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-  Typography,
-} from "@material-tailwind/react";
+import { Accordion, AccordionHeader, AccordionBody, Typography } from "@material-tailwind/react";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const faqs = [
   {
-    question:
-      "Mengapa beberapa stasiun tidak muncul pada peta tren, meskipun lot time seri tersedia?",
+    question: "Mengapa beberapa stasiun tidak muncul pada peta tren, meskipun lot time seri tersedia?",
     answerType: "text",
-    answer:
-      "Anda dapat menghubungi PTSP BMKG Bengkulu melalui email, telepon, atau datang langsung ke kantor kami.",
+    answer: "Anda dapat menghubungi PTSP BMKG Bengkulu melalui email, telepon, atau datang langsung ke kantor kami.",
   },
   {
-    question:
-      "Bagaimana cara mengajukan permohonan layanan informasi di kantor PTSP BMKG?",
+    question: "Bagaimana cara mengajukan permohonan layanan informasi di kantor PTSP BMKG?",
     answerType: "list",
     answer: [
       "Datang langsung ke Pelayanan Terpadu Satu Pintu (PTSP) BMKG di Jalan Angkasa I no. 2 Kemayoran Jakarta Pusat. Gedung E, Kirim email ke : ptsp[at]bmkg.go.id , atau kirim fax ke 021-65867063 dengan membawa :Surat Permohonan yang ditanda tangani (jika dari perusahaan, dibubuhkan stempel perusahaan)Surat tugas (jika dari perusahaan menugaskan org lain untuk mengurus permohonan ke BMKG)Fotokopi KTP pemohon.",
@@ -26,7 +18,7 @@ const faqs = [
       "Melakukan pembayaran (khusus untuk permohonan analisa cuaca, pembayaran dilakukan di awal masuk berkas permohonan), dengan cara:Pembayaran tunai ke bendahara penerima yang bertugas di PTSP.Transfer ke rek. Bendahara Penerima BMKG (jika sudah melakukan transfer harus mengirimkan bukti transfernya ke PTSP)",
       "Jika berkas permohonan sudah lengkap, permohonan akan diproses dalam waktu kurang lebih 14 hari kerja, khusus analisa cuaca, pengerjaannya kurang lebih 14 hari kerja per lokasi per harinya.",
       "Jika hasil sudah ada, petugas PTSP akan segera menghubungi pemohon melalui telepon atau email, untuk mengambil hasil di kantor Pelayanan Terpadu Satu Pintu (PTSP).",
-      "Pembayaran khusus untuk kalibrasi alat Meteorologi, Klimatologi, dan Geofisika : Membayar sesuai total tagihan kalibrasi alat yang akan diambil pemohon berdasarkan nomor order alat yang diperoleh dari lab.kalibrasi.BMKG Bayar langsung/tunai di PTSPBerkas lengkap sebagai dasar untuk pengambilan alat dan sertifikat di lab. kalibrasi.",
+      "Pembayaran khusus untuk kalibrasi alat Meteorologi, Klimatologi, dan Geofisika : Membayar sesuai total tagihan kalibrasi alat yang akan diambil pemohon berdasarkan nomor order alat yang diperoleh dari lab.kalibrasi.BMKG Bayar langsung/tunai di PTSP Berkas lengkap sebagai dasar untuk pengambilan alat dan sertifikat di lab. kalibrasi.",
     ],
   },
   {
@@ -43,14 +35,12 @@ const faqs = [
   {
     question: "Bagaimanakah jam pelayanan PTSP BMKG ?",
     answerType: "text",
-    answer:
-      "Hari : Senin s/d Jumat, waktu : 09.00 s/d 15.00 WIB, dan istirahat : 12.00 s/d 13.00 WIB.",
+    answer: "Hari : Senin s/d Jumat, waktu : 09.00 s/d 15.00 WIB, dan istirahat : 12.00 s/d 13.00 WIB.",
   },
   {
     question: "Melalui apa saja Pelanggan dapat mengontak PTSP BMKG ?",
     answerType: "text",
-    answer:
-      "Pelanggan dapat mengontak PTSP BMKG melalui Online Chat di website resmi : http://ptsp.bmkg.go.id, WA : 081382321504,  telepon : (021) 65867063, fax : (021) 4246703, atau email : ptsp.bmkg[at]bmkg.go.id.",
+    answer: "Pelanggan dapat mengontak PTSP BMKG melalui Online Chat di website resmi : http://ptsp.bmkg.go.id, WA : 081382321504,  telepon : (021) 65867063, fax : (021) 4246703, atau email : ptsp.bmkg@bmkg.go.id.",
   },
 ];
 
@@ -75,11 +65,7 @@ export default function FAQ() {
             <Typography variant="h6" className="flex-1">
               {faq.question}
             </Typography>
-            <IoIosArrowDropdownCircle
-              className={`ml-2 transform transition-transform ${
-                open === index ? "rotate-180" : "rotate-0"
-              }`}
-            />
+            <IoIosArrowDropdownCircle className={`ml-2 transform transition-transform ${open === index ? "rotate-180" : "rotate-0"}`} />
           </AccordionHeader>
           <AccordionBody className="p-3 bg-gray-100 rounded-lg">
             {faq.answerType === "text" && (
@@ -90,10 +76,7 @@ export default function FAQ() {
             {faq.answerType === "list" && (
               <ol className="list-decimal list-inside space-y-2">
                 {faq.answer.map((item, idx) => (
-                  <li
-                    key={idx}
-                    className="text-gray-700 text-base text-justify"
-                  >
+                  <li key={idx} className="text-gray-700 text-base text-justify">
                     {item.split("\n").map((line, i) => (
                       <span key={i}>
                         {line}
