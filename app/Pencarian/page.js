b6db13c26472cc36a659754c2714d30c0bbc424c
Rezkyindri31 <147515669+Rezkyindri31@users.noreply.github.com>
2025-05-15 21:52:08 +0700

    fix: Perbaikan Pencarian Deploy.

--- app/Pencarian/page.js
+++ app/Pencarian/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import HeaderTemplate from "@/components/HeaderTemplate";
 import TampilanPencarian from "@/app/Pencarian/components/Pencarian";
@@ -10,7 +11,9 @@ function Pencarian() {
       <Toaster position="top-right" reverseOrder={false} />
       <Navbar />
       <HeaderTemplate />
-      <TampilanPencarian />
+      <Suspense fallback={<div>Memuat hasil pencarian...</div>}>
+        <TampilanPencarian />
+      </Suspense>
       <Footer />
     </div>
   );
