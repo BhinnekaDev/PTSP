6ef39f1a59d09bdbbbe026ab6549cd662e5f9eaa
Rezkyindri31 <147515669+Rezkyindri31@users.noreply.github.com>
2026-03-28 02:56:31 +0700

    Feat: Hyperlink to PTSP Mobile.

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,23 @@
 "use client";
 
+import Image from "next/image";
 import { Typography } from "@/app/MTailwind";
+import UnduhAPK from "@/assets/img/Icon/Unduh-APK.png";
 import { FaInstagram, FaTelegramPlane, FaGlobe } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="bg-darkgray text-white lg:py-20 py-10 z-10 relative">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-5 gap-8">
         <div className="text-center lg:text-left">
           <Typography className="font-semibold mb-3 lg:text-lg text-base">
-            ©2024 PTSP BMKG BENGKULU
+            PTSP BMKG BENGKULU
           </Typography>
           <hr className="border-t-2 border-primary w-full mx-auto md:mx-0 mb-4" />
           <Typography className="lg:text-base text-sm">
-            PTSP BMKG Provinsi Bengkulu merupakan lembaga penyedia jasa atau
-            informasi yang berada di wilayah Bengkulu dan sekitarnya yang
-            berhubungan dengan klimatologi, meteorologi dan geofisika
+            PTSP BMKG Provinsi Bengkulu adalah layanan informasi dan jasa di
+            bidang klimatologi, meteorologi, dan geofisika untuk wilayah
+            Bengkulu dan sekitarnya.
           </Typography>
         </div>
         <div className="text-center lg:text-center">
@@ -24,10 +26,14 @@ function Footer() {
           </Typography>
           <hr className="border-t-2 border-primary w-full mx-auto md:mx-0 mb-4" />
           <Typography className="lg:text-base text-sm">
-            Jl. Depati Payung Negara, Pekan Sabtu, Kec. Selebar, Kota Bengkulu,
-            Bengkulu 38213
+            Jl. Depati Payung Negara, Komplek Bandar Udara Fatmawati Soekarno
+            Bengkulu.Kel. Pekan Sabtu, Kec. Selebar, Kota Bengkulu, Bengkulu –
+            38213
           </Typography>
-          <Typography className="text-base mt-2">0736-51064</Typography>
+          <Typography className="text-base mt-2">08117328773</Typography>
+          <Typography className="lg:text-base text-sm mt-2">
+            stamet.fatmawati@bmkg.go.id
+          </Typography>
         </div>
         <div className="text-center lg:text-center">
           <Typography className="font-semibold mb-3 lg:text-lg text-base">
@@ -35,13 +41,14 @@ function Footer() {
           </Typography>
           <hr className="border-t-2 border-primary w-full mx-auto md:mx-0 mb-4" />
           <Typography className="lg:text-base text-sm">
-            Jl. Ir. Rustandi Sugianto P. Baai Bengkulu 39172
+            Jl. R.E. Martadinata, Kelurahan Kandang, Kecamatan Kampung Melayu,
+            Kota Bengkulu, Bengkulu - 38216
           </Typography>
           <Typography className="lg:text-base text-sm mt-2">
-            0736-51251 / 0736-51426 / 0736-53030
+            08117321291
           </Typography>
           <Typography className="lg:text-base text-sm mt-2">
-            staklim.pulaubaai@bmkg.go.id
+            staklim.bengkulu@bmkg.go.id
           </Typography>
         </div>
         <div className="text-center lg:text-center">
@@ -50,14 +57,40 @@ function Footer() {
           </Typography>
           <hr className="border-t-2 border-primary w-full mx-auto md:mx-0 mb-4" />
           <Typography className="lg:text-base text-sm">
-            Jl. Pembangunan No. 156 Pasar Ujung, Kepahyang, Bengkulu
+            Kantor Bengkulu : Jl. Jati No. 37, Sawah Lebar Kec. Ratu Samban,
+            Kota Bengkulu, Bengkulu 38221 (depan McD)
+          </Typography>
+          <Typography className="lg:text-base text-sm">
+            Kantor Kepahiang: Jl. Pembangunan No. 156, Pasar Ujung, Kepahiang,
+            Kec. Kepahiang, Bengkulu
           </Typography>
           <Typography className="lg:text-base text-sm mt-2">
-            0732-391600
+            08111360636
           </Typography>
           <Typography className="lg:text-base text-sm mt-2">
             stageof.kepahiang@bmkg.go.id
           </Typography>
+        </div>
+        <div className="text-center lg:text-right">
+          <Typography className="font-semibold mb-3 lg:text-lg text-base uppercase">
+            Unduh PTSP Mobile
+          </Typography>
+          <hr className="border-t-2 border-primary w-full mx-auto md:mx-0 mb-4" />
+          <a
+            href="https://drive.google.com/drive/folders/1l3JFfEyPqYtCpyFR5iSyGf9aSYAbN4Yk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={UnduhAPK}
+              alt="unduh apk"
+              className="lg:w-full lg:h-1/5 rounded-xl"
+              width={130}
+              height={150}
+              quality={100}
+              priority
+            />
+          </a>
         </div>
       </div>
       <div className="border-t border-primary mt-10 pt-4">
@@ -82,7 +115,7 @@ function Footer() {
               className="cursor-pointer transform transition-transform duration-200 hover:scale-110"
               onClick={() =>
                 window.open(
-                  "https://l.instagram.com/?u=https%3A%2F%2Ft.me%2Fjoinchat%2FWKhKhIs5nORrQyOo&e=AT2_gidXz6KROzub3JWaazmiFwm01gfzeWlYi_zTkrKNkSNB5zIOUkpMh38ZbNK9NNBaGMlxvtSIrQxhtEXiNlwc6Ulz-QmS"
+                  "https://l.instagram.com/?u=https%3A%2F%2Ft.me%2Fjoinchat%2FWKhKhIs5nORrQyOo&e=AT2_gidXz6KROzub3JWaazmiFwm01gfzeWlYi_zTkrKNkSNB5zIOUkpMh38ZbNK9NNBaGMlxvtSIrQxhtEXiNlwc6Ulz-QmS",
                 )
               }
             />
